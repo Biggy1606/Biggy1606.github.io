5c6893c58d0c5d636b7e0e63e42f8f3e9a718b2c
Biggy1606 <igor1606553@gmail.com>
2020-05-26 15:28:19 +0200

    Big changes :)

--- js/date.js
+++ js/date.js
@@ -1,14 +1,39 @@
 var now = new Date();
-var days = new Array('Niedziela','Poniedziałek','Wtorek','roda','Czwartek','Piątek','Sobota');
-var months = new Array('Styczeń','Luty','Marzec','Kwiecień','Maj','Czerwiec','Lipiec','Sierpień','Wrzesień','Pażdziernik','Listopad','Grudzień');
-var date = ((now.getDate()<10) ? "0" : "")+ now.getDate();
+var days = new Array(
+	"Niedziela",
+	"Poniedziałek",
+	"Wtorek",
+	"roda",
+	"Czwartek",
+	"Piątek",
+	"Sobota"
+);
+var months = new Array(
+	"Styczeń",
+	"Luty",
+	"Marzec",
+	"Kwiecień",
+	"Maj",
+	"Czerwiec",
+	"Lipiec",
+	"Sierpień",
+	"Wrzesień",
+	"Pażdziernik",
+	"Listopad",
+	"Grudzień"
+);
+var date = (now.getDate() < 10 ? "0" : "") + now.getDate();
 var today = String;
 
-function fourDigitYear(number)	{
-	return (number < 1000) ? number + 1900 : number;
+function fourDigitYear(number) {
+	return number < 1000 ? number + 1900 : number;
 }
-today = days[now.getDay()] + ", " +
-        months[now.getMonth()] + " " +
-        date + ", " +
-        (fourDigitYear(now.getYear())) ;
+today =
+	days[now.getDay()] +
+	", " +
+	months[now.getMonth()] +
+	" " +
+	date +
+	", " +
+	fourDigitYear(now.getYear());
 document.write(today);
